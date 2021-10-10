9ee3b5b696782a93c972ccf61556e2a84108a349
syaisyah <terraaisyah@gmail.com>
2021-10-10 20:49:35 +0700

    tuple

--- starting-project/app.ts
+++ starting-project/app.ts
@@ -2,15 +2,18 @@
 interface Person {
   name: string;
   age: number;
-  hobbies: string[]; // any[]  ---> array of any datatype 
-};
+  hobbies: string[]; // any[]  ---> array of any datatype,
+  contohTuple: [ number, string] //tupe ---> array with fixed length and type
+}
 
 const person: Person = {
   name: "Aisyah",
   age: 20,
   hobbies: ["Eat", "Drink", "Sleep"],
+  contohTuple: [1, 'hai']
 };
 
-for (let hobby of person.hobbies) {
-  console.log(hobby)
-}
+person.contohTuple.push('hello') // untuk push itu pengecualian (dibolehin)
+
+console.log(person.contohTuple)
+
